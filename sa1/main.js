c79241edba95e2233e1c2e249ca3c0185008f181
kenNg1 <kng1230@hotmail.com>
2017-02-07 18:32:01 +0800

    End of day 2
    Recreated website structure and function works

---
+++ sa1/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  $("#button").click(function(){
+    $('ul').empty();
+    var number = $('input').val();
+    for(i=1;i<=number;i++){
+      if(i%3===0&&i%5===0){
+        $('ul').append('<li>pingpong</li>');
+      }
+      else if(i%3===0){
+        $('ul').append('<li>pong</li>');
+      }
+      else if(i%5===0){
+        $('ul').append('<li>ping</li>');
+      }
+      else {
+        $('ul').append('<li>'+i+'</li>');
+      }
+    };
+});
+});
+
+//
+
+
+
+
+
+
+//
